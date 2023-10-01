44ae584e12e4b72e17be9d5c9bceb218bd927859
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-01 12:11:21 +0300

    Modified ConstructionCompany class to account for custom houses

--- BuilderExercise/Models/ConstructionCompany.cs
+++ BuilderExercise/Models/ConstructionCompany.cs
@@ -10,6 +10,16 @@
             builder.BuildFoundation();
             builder.BuildWalls();
             builder.BuildRoof();
+
+            // There is probably a better way to do this, but for now, requires
+            // the least changing of code.
+            if (builder.GetType().Name == "CustomHouseBuilder")
+            {
+                builder.BuildGarage();
+                builder.BuildGarden();
+                builder.BuildSwimmingPool();
+            }
+
             return builder.GetHouse();
         }
     }
