838d331de139508cb1b737a51fbdd46df29b9744
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-01 11:45:34 +0300

    Added BrickHouseBuilder class

---
+++ BuilderExercise/Models/BrickHouseBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace BuilderExercise.Models
+{
+    public class BrickHouseBuilder : HouseBuilder
+    {
+        public override void BuildFoundation()
+        {
+            house.Foundation = "Piles of bricks";
+        }
+
+        public override void BuildWalls()
+        {
+            house.Walls = "Bricks and glass";
+        }
+
+        public override void BuildRoof()
+        {
+            house.Roof = "Brick Roof";
+        }
+
+        public override void BuildGarage()
+        {
+            house.HasGarage = true;
+        }
+
+        public override void BuildSwimmingPool()
+        {
+            house.HasSwimmingPool = false;
+        }
+
+        public override void BuildGarden()
+        {
+            house.HasGarden = true;
+        }
+    }
+}
