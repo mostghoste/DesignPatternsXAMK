9a503ba860ab8d90c73b3d343c2d6579038cdf0e
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-02 16:49:28 +0300

    Adjusted RamenBowl class to support multiple toppings and no extras

--- BuilderAssignment/Models/RamenBowl.cs
+++ BuilderAssignment/Models/RamenBowl.cs
@@ -16,17 +16,34 @@ namespace BuilderAssignment.Models
 
         public BrothType Broth { get; set; }
         public NoodleType Noodle { get; set; }
-        public Topping Topping { get; set; }
-        public Extra Extra { get; set; }
+        public List<Topping> Topping { get; set; }
+        public Extra? Extra { get; set; }
 
         //TODO#2: Return description of finished bowl of ramen
         public string FinishedBowlOfRamen()
         {
+            // Turn topping list into a single string.
+            string toppings;
+            if (Topping.Count > 0)
+            {
+                toppings = Topping[0].ToString();
+                for (int i = 1; i < Topping.Count; i++)
+                {
+                    toppings += "; " + Topping[i].ToString();
+                }
+            }
+            else
+            {
+                toppings = "None";
+            }
+
+            string extra = Extra != null ? Extra.ToString() : "None";
+
             string ramen = String.Format(" Broth: {0}\n Noodle: {1}\n Topping: {2}\n Extra: {3}",
                 Broth.ToString(),
                 Noodle.ToString(),
-                Topping.ToString(),
-                Extra.ToString()
+                toppings,
+                extra
                 );
             return ramen;
         }
