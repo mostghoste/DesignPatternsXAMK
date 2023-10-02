76c06944ba388c2e075273113faaf6a3b5e72d28
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-02 16:58:09 +0300

    Added VeganRamenBuilder and preparation for vegan ramen

---
+++ BuilderAssignment/Builders/VeganRamenBuilder.cs
@@ -0,0 +1,44 @@
+﻿using BuilderAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderAssignment.Builders
+{
+    internal class VeganRamenBuilder : IRamenBuilder
+    {
+        /// - VeganRamenBuilder
+        ///     Recepie: 
+        ///     - Broth: Vegan
+        ///     - Noodles: Soba
+        ///     - Toppings: Bambo Shoots & Seaweed
+        ///     - Extras: no extras
+        private RamenBowl ramen = new RamenBowl();
+
+        public void AddBroth()
+        {
+            ramen.Broth = BrothType.Vegan;
+        }
+        public void AddNoodles()
+        {
+            ramen.Noodle = NoodleType.Soba;
+        }
+        public void AddToppings()
+        {
+            ramen.Topping.Add(Topping.BambooShoots);
+            ramen.Topping.Add(Topping.Seaweed);
+        }
+
+        public void AddExtras()
+        {
+            ramen.Extra = null;
+        }
+
+        public RamenBowl GetRamen()
+        {
+            return ramen;
+        }
+    }
+}
--- BuilderAssignment/Program.cs
+++ BuilderAssignment/Program.cs
@@ -50,14 +50,17 @@ namespace BuilderAssignment
             // TODO#2: Create new RamenBuilder's for each ramen recepies (MisoRamenBuilder, SpicyRamenBuilder, VeganRamenBuilder)
             IRamenBuilder misoBuilder = new MisoRamenBuilder();
             IRamenBuilder spicyBuilder = new SpicyRamenBuilder();
+            IRamenBuilder veganBuilder = new VeganRamenBuilder();
 
             // TODO#3: Use your RamenChef to prepare (build) each type of ramen (Miso, Spicy, Vegan)
             RamenBowl misoRamen = chef.PrepareRamen(misoBuilder);
             RamenBowl spicyRamen = chef.PrepareRamen(spicyBuilder);
+            RamenBowl veganRamen = chef.PrepareRamen(veganBuilder);
             // TODO#4: After the RamenCheft has prepared the bowl of ramen, use FinishedBowlOfRamen to get the content of
             //         the RamenBowl and print it out using WriteLine method
             Console.WriteLine("Miso ramen:\n" + misoRamen.FinishedBowlOfRamen() + "\n");
             Console.WriteLine("Spicy ramen:\n" + spicyRamen.FinishedBowlOfRamen() + "\n");
+            Console.WriteLine("Vegan ramen:\n" + veganRamen.FinishedBowlOfRamen() + "\n");
         }
     }
 }
