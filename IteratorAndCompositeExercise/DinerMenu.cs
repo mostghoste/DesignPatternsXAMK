9940ff048dda36e663bff8124adeed7f6223f00d
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-09 07:46:40 +0300

    Moved DinerMenu class outside of program

---
+++ IteratorAndCompositeExercise/DinerMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static IteratorAndCompositeExercise.Program;
+
+namespace IteratorAndCompositeExercise
+{
+    internal class DinerMenu
+    {
+        private int Max_Items = 6;
+        int numberOfItems = 0;
+        MenuItem[] MenuItems;
+
+        public DinerMenu()
+        {
+            MenuItems = new MenuItem[Max_Items];
+
+            AddItem("Vegetarian BLT", "(Fakin´) Bacon with lettuce & tomato on whole wheat", true, 2.99);
+        }
+
+        public void AddItem(string name, string description, bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            if (numberOfItems >= Max_Items)
+            {
+                Console.WriteLine("Sorry, menu is full! Can't add item to menu");
+            }
+            else
+            {
+                MenuItems[numberOfItems] = menuItem;
+                numberOfItems++;
+            }
+        }
+
+        public MenuItem[] GetMenuItems()
+        {
+            return MenuItems;
+        }
+    }
+}
--- IteratorAndCompositeExercise/Program.cs
+++ IteratorAndCompositeExercise/Program.cs
@@ -70,37 +70,5 @@ namespace IteratorAndCompositeExercise
                 return MenuItems;
             }
         }
-
-        public class DinerMenu
-        {
-            private int Max_Items = 6;
-            int numberOfItems = 0;
-            MenuItem[] MenuItems;
-
-            public DinerMenu()
-            {
-                MenuItems = new MenuItem[Max_Items];
-
-                AddItem("Vegetarian BLT", "(Fakin´) Bacon with lettuce & tomato on whole wheat", true, 2.99);
-            }
-
-            public void AddItem(string name, string description, bool vegetarian, double price)
-            {
-                MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-                if (numberOfItems >= Max_Items)
-                {
-                    Console.WriteLine("Sorry, menu is full! Can't add item to menu");
-                } else
-                {
-                    MenuItems[numberOfItems] = menuItem;
-                    numberOfItems++;
-                }
-            }
-
-            public MenuItem[] GetMenuItems()
-            {
-                return MenuItems;
-            }
-        }
     }
 }
