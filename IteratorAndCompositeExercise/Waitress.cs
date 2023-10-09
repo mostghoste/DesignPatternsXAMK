b62347d2ce4eb7783f2834acbeecc1570b1def43
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-09 08:03:28 +0300

    Created waitress class

---
+++ IteratorAndCompositeExercise/Waitress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorAndCompositeExercise
+{
+    internal class Waitress
+    {
+        DinerMenu dinerMenu;
+        
+        public Waitress(DinerMenu dinerMenu)
+        {
+            this.dinerMenu = dinerMenu;
+        }
+
+        public void printMenu()
+        {
+            Iterator dinerIterator = dinerMenu.createIterator();
+            Console.WriteLine("--- Diner menu ---");
+            printMenu(dinerIterator);
+        }
+
+        private void printMenu(Iterator iterator)
+        {
+            while (iterator.hasNext())
+            {
+                MenuItem menuItem = (MenuItem) iterator.next();
+                Console.WriteLine(menuItem.ToString());
+            }
+        }
+    }
+}
