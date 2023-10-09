7ff8369ca04742dbfb9d7b610ce4b0468564d0a6
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-09 07:55:06 +0300

    Implemented DinerMenuIterator

--- IteratorAndCompositeExercise/DinerMenu.cs
+++ IteratorAndCompositeExercise/DinerMenu.cs
@@ -39,4 +39,30 @@ namespace IteratorAndCompositeExercise
             return MenuItems;
         }
     }
+
+    internal class DinerMenuIterator : Iterator
+    {
+        MenuItem[] items;
+        int position = 0;
+
+        public DinerMenuIterator(MenuItem[] items)
+        {
+            this.items = items;
+        }
+
+        public bool hasNext()
+        {
+            if (position >= items.Length || items[position] == null)
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+        public object next()
+        {
+            MenuItem menuItem = items[position++];
+            return menuItem;
+        }
+    }
 }
