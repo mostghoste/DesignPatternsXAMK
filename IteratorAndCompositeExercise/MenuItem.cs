06489a9efef8d7638ece0df22068dcba7ee17429
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-10-09 07:48:48 +0300

    Moved menuItem class out of program

---
+++ IteratorAndCompositeExercise/MenuItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorAndCompositeExercise
+{
+    internal class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Vegetarian { get; set; }
+        public double Price { get; set; }
+
+        public MenuItem(string name, string description, bool vegetarian, double price)
+        {
+            Name = name;
+            Description = description;
+            Vegetarian = vegetarian;
+            Price = price;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetDescription()
+        {
+            return Description;
+        }
+
+        public Boolean GetVegetarian()
+        {
+            return Vegetarian;
+        }
+
+        public double GetPrice()
+        {
+            return Price;
+        }
+    }
+}
--- IteratorAndCompositeExercise/Program.cs
+++ IteratorAndCompositeExercise/Program.cs
@@ -11,41 +11,5 @@ namespace IteratorAndCompositeExercise
         {
             Console.WriteLine("Hello, World!");
         }
-
-        public class MenuItem
-        {
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public bool Vegetarian { get; set; }
-            public double Price { get; set; }
-
-            public MenuItem(string name, string description, bool vegetarian, double price) 
-            { 
-                Name = name;
-                Description = description;
-                Vegetarian = vegetarian;
-                Price = price;
-            }
-
-            public string GetName()
-            {
-                return Name;
-            }
-
-            public string GetDescription()
-            {
-                return Description;
-            }
-
-            public Boolean GetVegetarian()
-            {
-                return Vegetarian;
-            }
-
-            public double GetPrice()
-            {
-                return Price;
-            }
-        }
     }
 }
