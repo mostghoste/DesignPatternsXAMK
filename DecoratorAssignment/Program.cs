e0ebf098155eebfff73161b5c50a60fb0c4d54b5
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-02 09:31:39 +0200

    added Decorator template files

---
+++ DecoratorAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DecoratorAssignment
+{
+    // Instructions
+    // You can implement your whole solution here
+    // Optionally you can use folder structure if you deem it necessary
+    // For this Assignment we will use Decorator pattern example introduced in the book Head First Design Patterns by O'Reilly
+    // Chapeter 3 the DecoratorPattern: Decorating Objects (starts at page 79)
+    // Link to pdf: https://github.com/ajitpal/BookBank/blob/master/%5BO%60Reilly.%20Head%20First%5D%20-%20Head%20First%20Design%20Patterns%20-%20%5BFreeman%5D.pdf
+    // NOTE: Remember that the code examples in this book are written in java so you can't just copy the code examples given there
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
