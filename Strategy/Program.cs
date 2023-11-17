b0d0b7cfdc310b2fdcca524601fbe2bd46ee7206
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-17 16:23:04 +0200

    Added Bird and MoveStrategy interfaces

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,5 +1,19 @@
 ﻿namespace Strategy
 {
+    public interface Bird
+    {
+        string name { get; set; }
+        MoveStrategy moveStrategy { get; set; }
+
+        void move();
+        void setStrategy(MoveStrategy moveStrategy);
+    }
+
+    public interface MoveStrategy
+    {
+        void execute();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
