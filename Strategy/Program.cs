c0ed3fbe105dc6a0a29da519e3149dd602a8ca3b
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-17 16:47:24 +0200

    Added SwimStrategy, RunStrategy, Penguin and Ostrich classes

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -21,6 +21,20 @@
             Console.WriteLine("Flaps its wings and soars through the air!");
         }
     }
+    public class SwimStrategy : MoveStrategy
+    {
+        public void execute()
+        {
+            Console.WriteLine("Uses its little feet to propel itself in the water!");
+        }
+    }
+    public class RunStrategy : MoveStrategy
+    {
+        public void execute()
+        {
+            Console.WriteLine("Uses its magnificent legs to walk on the land!");
+        }
+    }
 
     public class Sparrow : Bird
     {
@@ -44,21 +58,72 @@
         }
     }
 
+    public class Penguin : Bird
+    {
+        public override string name { get; set; }
+        public override MoveStrategy moveStrategy { get; set; }
+
+        public Penguin(MoveStrategy moveStrategy)
+        {
+            name = "Penguin";
+            this.moveStrategy = moveStrategy;
+        }
+        public override void Move()
+        {
+            Console.Write(name + " movement: ");
+            moveStrategy.execute();
+        }
+
+        public override void SetStrategy(MoveStrategy moveStrategy)
+        {
+            this.moveStrategy = moveStrategy;
+        }
+    }
+
+    public class Ostrich : Bird
+    {
+        public override string name { get; set; }
+        public override MoveStrategy moveStrategy { get; set; }
+
+        public Ostrich(MoveStrategy moveStrategy)
+        {
+            name = "Ostrich";
+            this.moveStrategy = moveStrategy;
+        }
+        public override void Move()
+        {
+            Console.Write(name + " movement: ");
+            moveStrategy.execute();
+        }
+
+        public override void SetStrategy(MoveStrategy moveStrategy)
+        {
+            this.moveStrategy = moveStrategy;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             // Create all the different types of birds
             Bird sparrow = new Sparrow(new FlyStrategy());
+            Bird penguin = new Penguin(new SwimStrategy());
+            Bird ostrich = new Ostrich(new RunStrategy());
 
             // Initialise the list of birds with default values
-            List<Bird> birdList = new List<Bird> { sparrow };
+            List<Bird> birdList = new List<Bird> { sparrow, penguin, ostrich };
 
             // Make every bird in the list move
             foreach (Bird bird in birdList)
             {
                 bird.Move();
             }
+
+            // Display the capability of changing movement strategy at runtime
+            Console.WriteLine("\nLooks like the penguin has reached the shore!");
+            penguin.SetStrategy(new RunStrategy());
+            penguin.Move();
         }
     }
 }
