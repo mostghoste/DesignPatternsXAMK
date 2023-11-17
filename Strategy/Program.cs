53b7706374aa0bdc1c890f3f0eb8fa2e2f185907
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-17 16:42:40 +0200

    Converted Bird from interface to abstract class, implemented the Sparrow and FlyStrategy concrete classes

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,12 +1,12 @@
 ﻿namespace Strategy
 {
-    public interface Bird
+    public abstract class Bird
     {
-        string name { get; set; }
-        MoveStrategy moveStrategy { get; set; }
+        public abstract string name { get; set; }
+        public abstract MoveStrategy moveStrategy { get; set; }
 
-        void move();
-        void setStrategy(MoveStrategy moveStrategy);
+        public abstract void Move();
+        public abstract void SetStrategy(MoveStrategy moveStrategy);
     }
 
     public interface MoveStrategy
@@ -14,11 +14,51 @@
         void execute();
     }
 
+    public class FlyStrategy : MoveStrategy
+    {
+        public void execute()
+        {
+            Console.WriteLine("Flaps its wings and soars through the air!");
+        }
+    }
+
+    public class Sparrow : Bird
+    {
+        public override string name { get; set; }
+        public override MoveStrategy moveStrategy { get; set; }
+
+        public Sparrow(MoveStrategy moveStrategy)
+        {
+            name = "Sparrow";
+            this.moveStrategy = moveStrategy;
+        }
+        public override void Move()
+        {
+            Console.Write(name + " movement: ");
+            moveStrategy.execute();
+        }
+
+        public override void SetStrategy(MoveStrategy moveStrategy)
+        {
+            this.moveStrategy = moveStrategy;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create all the different types of birds
+            Bird sparrow = new Sparrow(new FlyStrategy());
+
+            // Initialise the list of birds with default values
+            List<Bird> birdList = new List<Bird> { sparrow };
+
+            // Make every bird in the list move
+            foreach (Bird bird in birdList)
+            {
+                bird.Move();
+            }
         }
     }
 }
