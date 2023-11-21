78e55c7919b37ed58882da6dc707c03d0bcb7559
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-21 20:14:37 +0200

    Add: Light class

--- Command/Program.cs
+++ Command/Program.cs
@@ -4,6 +4,19 @@
     {
         void execute();
     }
+
+    public class Light
+    {
+        void on()
+        {
+            Console.WriteLine("Light is on");
+        }
+        void off()
+        {
+            Console.WriteLine("Light is off");
+        
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
