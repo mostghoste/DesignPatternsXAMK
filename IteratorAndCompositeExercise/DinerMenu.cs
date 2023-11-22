1208c055d16a444e9162e9ed58334da42ff6e401
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-22 12:59:49 +0200

    Add: Additional items to DinerMenu constructor

--- IteratorAndCompositeExercise/DinerMenu.cs
+++ IteratorAndCompositeExercise/DinerMenu.cs
@@ -18,6 +18,11 @@ namespace IteratorAndCompositeExercise
             MenuItems = new MenuItem[Max_Items];
 
             AddItem("Vegetarian BLT", "(Fakin´) Bacon with lettuce & tomato on whole wheat", true, 2.99);
+            AddItem("BLT", "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+            AddItem("Soup of the day","Soup of the day, with a side of potato salad", false, 3.29);
+            AddItem("Hotdog","A hot dog, with saurkraut, relish, onions, topped with cheese",false, 3.05);
+            AddItem("Steamed Veggies and Brown Rice","Steamed vegetables over brown rice", true, 3.99);
+            AddItem("Pasta","Spaghetti with Marinara Sauce, and a slice of sourdough bread",true, 3.89);
         }
 
         public void AddItem(string name, string description, bool vegetarian, double price)
