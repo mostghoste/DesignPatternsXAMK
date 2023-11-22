50c0933750ce4782ba9e9814f8016c4edd2db2af
SarunasJacikas <fsaja018@edu.xamk.fi>
2023-11-22 13:01:39 +0200

    Add: Additional items to PancakeHouseMenu

--- IteratorAndCompositeExercise/PancakeHouseMenu.cs
+++ IteratorAndCompositeExercise/PancakeHouseMenu.cs
@@ -17,6 +17,9 @@ namespace IteratorAndCompositeExercise
             MenuItems = new ArrayList();
 
             AddItem("K&B´s Pancake Breakfast", "Pancakes with scrambled eggs, and toast", false, 2.99);
+            AddItem("Regular Pancake Breakfast","Pancakes with fried eggs, sausage",false,2.99);
+            AddItem("Blueberry Pancakes","Pancakes made with fresh blueberries, and blueberry syrup",true,3.49);
+            AddItem("Waffles","Waffles, with your choice of blueberries or strawberries",true,3.59);
         }
 
         public void AddItem(string name, string description, bool vegetarian, double price)
